5b13c0543eaf0a9181e7fe36c8a2692b03d6c5c0
iam-suryaprasad <msuryaprasad11@gmail.com>
2024-09-21 18:19:30 +0530

    all files are added

---
+++ terraform/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "s3-uploader"
+
+    workspaces {
+      name = "s3-uploader"
+    }
+  }
+  required_version = ">= 1.6.3"
+}
+
+provider "aws" {
+  region = var.region
+}
